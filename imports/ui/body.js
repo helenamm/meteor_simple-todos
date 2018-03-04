f37d47578780cd4c5bfc0344364e2f884370c628
Helena M.M <helena.mrtz@gmail.com>
2018-03-04 20:13:37 +0100

    LOGIN added

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,3 +1,4 @@
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
  
 import { Tasks } from '../api/tasks.js';
@@ -41,6 +42,8 @@ Template.body.events({
     Tasks.insert({
       text,
       createdAt: new Date(), // current time
+      owner: Meteor.userId(),
+      username: Meteor.user().username,
     });
  
     // Clear form
