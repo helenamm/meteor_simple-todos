6897dc8d63dec444f5d0acd665d0b5c6bba15343
Helena M.M <helena.mrtz@gmail.com>
2018-03-04 18:57:45 +0100

    state

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,15 +1,30 @@
 import { Template } from 'meteor/templating';
  
 import { Tasks } from '../api/tasks.js';
+import { ReactiveDict } from 'meteor/reactive-dict';
+
 
 import './task.js';
 import './body.html';
+
+
+Template.body.onCreated(function bodyOnCreated(){
+  this.state = new ReactiveDict();
+})
  
 Template.body.helpers({
   tasks() {
-    // Show newest tasks at the top
+    const instance = Template.instance();
+    if(instance.state.get('hideCompleted', event.target.checked)) {
+      //If hide completed is checked, filter task
+      return Tasks.find({ checked: { $ne: true} }, { sort : { createdAt: -1 } });
+    }
+    // Otherwise, return all the tasks 
     return Tasks.find({}, { sort: { createdAt: -1 } });
   },
+  incompleteCount(){
+    return Tasks.find( { checked :  {$ne: true}}).count();
+  }
 });
 
 //Event handler for form submit
@@ -30,5 +45,8 @@ Template.body.events({
  
     // Clear form
     target.text.value = '';
+  },
+  'change .hide-completed input'(event, instance){
+    instance.state.set('hideCompleted', event.target.checked)
   },
 });
